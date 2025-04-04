1a60c0862398cb9993a865d4a4fe955a4b64be30
ericksiu <erickcruzrobles@gmail.com>
2025-04-03 21:29:25 -0600

    Integradora

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  verYoutube(){
+    window.open('https://youtu.be/kEF-QMiLHxU?si=_rw3VlvjkNaLJzyw');
+  }
   mostrarMas = false; 
   textoCorto = 'El sol brillaba sobre la selva cuando la familia de nuestro valiente capibara decidió ir al río a refrescarse. Pero él, el más pequeño de todos, se quedó dormido bajo la sombra de un árbol. Al despertar...'; 
   textoLargo = 'desesperado, buscó a su familia por todas partes hasta que, en la distancia, vio una camioneta alejándose a toda velocidad. Sin dudarlo, su instinto lo llevó a lanzarse en una persecución frenética. Los cazadores furtivos se habían llevado a su familia, y él no se detendría hasta rescatarlos. Su viaje lo llevará a través de densos bosques, peligrosas carreteras y bulliciosas ciudades, esquivando obstáculos y enfrentando desafíos hasta llegar al oscuro almacén donde los traficantes de animales escondían a sus presas. La aventura apenas comienza… ¿Podrá nuestro pequeño capibara superar cada obstáculo y salvar a su familia antes de que sea demasiado tarde?';
